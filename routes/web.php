58d5f46b812fd035bb612f8de20a4d66e8c83462
aunyamanee99 <aunyamanee@kkumail.com>
2022-05-05 15:35:50 +0700

    Changed route impression dashboard page for use query builder.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MemberController;
 use App\Http\Middleware\CheckAdmin;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,13 +36,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         //Query ผ่าน Model
+//         $users = User::orderBy('id', 'asc')->paginate(5);
+//         return view('dashboard', compact('users'));
+//     })->name('dashboard');
+// });
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        $users = User::orderBy('id', 'asc')->paginate(5);
+        //Query ผ่าน DB โดยตรง ไม่มี Model ใน project
+        //ไม่มี diffForHumans ให้ใช้โดยตรง ต้องไปใช้ Carbon\Carbon::parse($user->created_at)->diffForHumans() ในการเเปลงเวลา
+        $users = DB::table('users')->orderBy('id', 'asc')->paginate(5);
         return view('dashboard', compact('users'));
     })->name('dashboard');
 });
