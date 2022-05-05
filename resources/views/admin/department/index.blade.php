51abb08558d2958246b512f057bce4b9ba4772c5
aunyamanee99 <aunyamanee@kkumail.com>
2022-05-05 17:16:38 +0700

    Add form template in view department.

--- resources/views/admin/department/index.blade.php
+++ resources/views/admin/department/index.blade.php
@@ -1,21 +1,37 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Hello, {{Auth::user()->name}}
+            Hello, {{ Auth::user()->name }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="container">
             <div class="row">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        Hello Department
-                        
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Data table</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Form</div>
+                        <div class="card-body">
+                            <form action="" method="POST">
+                                <div class="form-group">
+                                    <label for="department_name">Rank</label><br>
+                                    <input type="text" class="form-contro" name="department_name">
+                                </div>
+                                <br>
+                                <input type="submit" value="save" class="btn btn-primary">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    </div>
     </div>
     </div>
 </x-app-layout>
